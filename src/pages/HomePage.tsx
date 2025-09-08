5af1920f3645ecb7b407d5beb6b027ddf1382b60
Yasirkhanktk <119963833+Yasirkhanktk@users.noreply.github.com>
2025-09-08 16:21:58 +0500

    revert

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -106,7 +106,7 @@ const HomePage = ({ navigateTo }) => {
               {/* Main image */}
               <div className="relative overflow-hidden rounded-3xl border-2 border-primary/30 glass-morphism">
                 <ImageWithFallback
-                  src="../src/Assets/yasir.png"
+                  src="../src/Assets/yas"
                   alt="Alex Chen - UI/UX Designer"
                   className="w-80 h-96 object-cover hover:scale-105 transition-transform duration-700"
                 />
